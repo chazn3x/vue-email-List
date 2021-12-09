2c3729be1d03c520e5f4b05be6105d9887f26187
Giovanni <gio.a.98@gmail.com>
2021-12-09 15:10:09 +0100

    add random password

--- js/main.js
+++ js/main.js
@@ -8,11 +8,19 @@ const app = new Vue({
     },
     created() {
         for (let i = 0; i < 10; i++) {
+            let newObj = {
+                mail: null,
+                psw: null,
+            }
             axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
             .then((response) => {
-                console.log(response.data.response);
-                this.mailList.push(response.data.response);
+                newObj.mail = response.data.response;
             });
+            axios.get("https://flynn.boolean.careers/exercises/api/random/word")
+            .then((response) => {
+                newObj.psw = response.data.response + Math.floor(Math.random() * 1000);
+            });
+            this.mailList.push(newObj);
         }
     },
 });
