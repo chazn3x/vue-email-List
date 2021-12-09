279e4adbc53eac07f8a1d0b612bbb751b0168ff9
Giovanni <gio.a.98@gmail.com>
2021-12-09 13:28:28 +0100

    fine con bonus (bug safari: mail tutte uguali)

--- js/main.js
+++ js/main.js
@@ -1,12 +1,18 @@
 const app = new Vue({
     el: "#root",
     data: {
-
+        mailList: [],
     },
     methods: {
 
     },
     created() {
-        
+        for (let i = 0; i < 10; i++) {
+            axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
+            .then((response) => {
+                console.log(response.data.response);
+                this.mailList.push(response.data.response);
+            });
+        }
     },
 });
