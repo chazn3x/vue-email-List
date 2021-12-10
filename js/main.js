3131402e8e350ccc92762baebf6f0d7e6b3cf449
Giovanni <gio.a.98@gmail.com>
2021-12-10 10:25:40 +0100

    fix bonus con oggetti

--- js/main.js
+++ js/main.js
@@ -15,12 +15,12 @@ const app = new Vue({
             axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
             .then((response) => {
                 newObj.mail = response.data.response;
+                axios.get("https://flynn.boolean.careers/exercises/api/random/word")
+                .then((response) => {
+                    newObj.psw = response.data.response + Math.floor(Math.random() * 1000);
+                    this.mailList.push(newObj);
+                });
             });
-            axios.get("https://flynn.boolean.careers/exercises/api/random/word")
-            .then((response) => {
-                newObj.psw = response.data.response + Math.floor(Math.random() * 1000);
-            });
-            this.mailList.push(newObj);
         }
     },
 });
